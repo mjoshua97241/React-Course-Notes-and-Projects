fe2ed6d5ce39eb293850ec1b3478ecdfb741a32c
mjoshua97241 <vargas.michaeljoshua@gmail.com>
2024-08-26 08:02:27 +0800

    Another Piece of State

--- src/App.js
+++ src/App.js
@@ -15,6 +15,9 @@ export default function App() {
   */
   const [step, setStep] = useState(1); // useState called **HOOKS** in React. useState can only be used at the **TOP LEVEL** of the component. Not inside the functions, if-else statements.
   // ONLY update the state **using "setter function (i.e. setStep)"**
+  // const [test, setTest] = useState({ name: "Jonas" });
+
+  const [isOpen, setIsOpen] = useState(true);
 
   // Function inside the component
   function handlePrevious() {
@@ -23,35 +26,51 @@ export default function App() {
 
   function handleNext() {
     if (step < 3) setStep(step + 1);
+
+    // BAD PRACTICE - mutating objects
+    // test.name = "Fred";
+
+    // setTest({ name: "Fred" });
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-      </div>
+    // use <div></div> to start JSX
+    // <> "React Fragment" when you want to use more than one element
+    <>
+      <button className="close" onClick={() => setIsOpen(!isOpen)}>
+        &times;
+      </button>
 
-      <p className="message">
-        Step {step}: {messages[step - 1]}
-      </p>
+      {isOpen && (
+        <div div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+          </div>
 
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-        >
-          Next
-        </button>
-      </div>
-    </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+            {/* {test.name} */}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
 
@@ -78,4 +97,24 @@ STATE
     2. Persist local variables between renders
 
     👋 State is a **tool**. Mastering state will unlock the power of React development.
+    - Always treat state immutable.
+
+    THE MECHANICS OF STATE IN REACT
+      - We don't do direct DOM manipulations
+        - How is a component view updated then?
+          - In React, a view is updated by re-rendering the component
+            - A component is re-rendered when its state is updated (Event Handler -> Update State -> Re-render 🔁)
+              - So to update a view, we update state
+    
+    💡 REACT **reacts** to state changes by re-rendering the UI
+  
+  2. How to use state in **practice**?
+    👉 useState
+    👉 useReducer
+    👉 Context API
+
+  3. **Thinking** about state
+    👉 When to use state
+    👉 Where to place state
+    👉 Types of state
 */
